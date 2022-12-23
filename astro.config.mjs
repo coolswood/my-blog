72662a40125403980725c9c6fe28067f65342238
Daniil Sugonyaev <daniil.sugonyaev@yolo.com>
2022-12-23 12:11:14 +0200

    partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,16 @@ import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   site: settings.site,
-  integrations: [sitemap(), mdx(), prefetch()],
+  integrations: [sitemap(), mdx(), prefetch(), partytown({
+    config: { 
+      forward: ["dataLayer.push"] 
+    },
+  })],
   vite: {
     ssr: {
       external: ["svgo"]
