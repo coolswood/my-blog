79d45ec7b86cab49506e77af5617298ab40201ef
Daniil Sugonyaev <daniil.sugonyaev@yolo.com>
2022-12-24 11:21:42 +0200

    robot txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import { settings } from './src/data/settings';
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 import prefetch from "@astrojs/prefetch";
@@ -12,7 +13,7 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   site: settings.site,
-  integrations: [sitemap(), mdx(), prefetch(), partytown({
+  integrations: [sitemap(), robotsTxt(), mdx(), prefetch(), partytown({
     config: { 
       forward: ["dataLayer.push"] 
     },
