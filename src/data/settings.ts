a5a229302e9a331cf579a6cfae658a2466751e50
Daniil Sugonyaev <daniil.sugonyaev@yolo.com>
2022-12-24 10:59:17 +0200

    update

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,7 +1,7 @@
 export const settings = {
   site: 'https://www.danielsugonyaev.com',
-  name: 'Блог Даниила Сугоняева',
-  title: 'Личный блог Даниила Сугоняева',
+  name: 'Облог о жизни, путешествиях и технологиях',
+  title: 'Облог о жизни, путешествиях и технологиях',
   description: 'Мой личный блог',
   showLittleSticksPlug: true,
 }
