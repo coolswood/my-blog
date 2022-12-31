47e175efd51869e719f0b37903b2707d3279e18a
Daniil Sugonyaev <daniil.sugonyaev@yolo.com>
2022-12-31 15:36:35 +0200

    i18

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { settings } from './src/data/settings';
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import robotsTxt from 'astro-robots-txt';
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 import prefetch from "@astrojs/prefetch";
@@ -10,7 +11,7 @@ import prefetch from "@astrojs/prefetch";
 // https://astro.build/config
 export default defineConfig({
   site: settings.site,
-  integrations: [sitemap(), robotsTxt(), mdx(), prefetch()],
+  integrations: [sitemap(), robotsTxt(), mdx(), prefetch(), astroI18next()],
   vite: {
     ssr: {
       external: ["svgo"]
